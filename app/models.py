915e881fdf4ed3a14da4909038b0f52f838f05e5
zipgids <zipgids.mkhize@gmail.com>
2026-09-20 17:35:14 +0200

    Update models.py
    Added Observations class

--- app/models.py
+++ app/models.py
@@ -1,4 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
+from decimal import Decimal
+
+class Observations(BaseModel):
+    #compulsory
+    treatment_id: int
+    plot_number: int = Field(ge=1)
+    rep: int = Field(ge=1)
+    device_id: str
+
+    #optional
+    plant_height_cm: Optional[Decimal] = Field(default=None, ge=0, le=4000, decimal_places=3)
+    yield_kg_per_plot: Optional[Decimal] = Field(default=None, ge=0, le=200, decimal_places=3)
+    spad: Optional[Decimal] = Field(default=None, ge=0.0, le=99.9, decimal_places=3)
+    disease_pct: Optional[Decimal] = Field(default=None, ge=0, le=100, decimal_places=3 )
+    root_mass_g: Optional[Decimal] = Field(default=None, ge=0, le=1000, decimal_places=3)
 
 
 class TreatmentStatistics(BaseModel):
